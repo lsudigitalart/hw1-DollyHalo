f7ceabeff4eeddfb24a0424d320b4dd189acfd89
HollySloat <157514488+DollyHalo@users.noreply.github.com>
2025-09-03 01:07:41 -0500

    changed title and location of 2nd elipse

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+  // Put things you want to run once in here. 
+}
+
+function draw() {
+    background(200);
+    ellipse(200, 200, 50, 50);
+    ellipse(400, 200, 50, 50);
+    ellipse(200, 200, 50, 50);
+    ellipse(200, 200, 50, 50);
+  // Put things you want to over and over here. 
+}
