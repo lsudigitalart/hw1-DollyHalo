b1fc08c7c97696b528318d2a868a660fc52338df
HollySloat <157514488+DollyHalo@users.noreply.github.com>
2025-09-17 16:29:55 -0500

    moved location of elipses

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ function setup() {
 function draw() {
     background(200);
     ellipse(200, 200, 50, 50);
-    ellipse(400, 200, 50, 50);
-    ellipse(700, 200, 50, 50);
-    ellipse(100, 200, 50, 50);
+    ellipse(250, 250, 50, 50);
+    ellipse(300, 150, 50, 50);
+    ellipse(150, 100, 50, 50);
   // Put things you want to over and over here. 
 }
